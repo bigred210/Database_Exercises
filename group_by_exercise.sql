a840b74ab05e1a1c8e4aa547d8bc3e8700b531ee
BIGRED <RED@Jerods-MacBook-Pro.local>
2016-05-09 11:23:10 -0500

    step6 complete for GROUP BY exercise

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -16,3 +16,9 @@ SELECT DISTINCT first_name, last_name
 FROM employees
 WHERE last_name LIKE 'E%'
     AND last_name LIKE '%e'
+
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+    AND last_name NOT LIKE '%qu%';
+
