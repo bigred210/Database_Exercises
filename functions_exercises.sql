796984e3a8a7b8d1db10b88e38868ee3b3ed5b08
BIGRED <RED@Jerods-MacBook-Pro.local>
2016-05-09 13:46:50 -0500

    step4 complete FUNCTIONS exercise

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -13,11 +13,11 @@ WHERE last_name LIKE 'E%'
     ORDER BY last_name DESC;
     
 
-SELECT first_name, last_name
+SELECT first_name, last_name,DATEDIFF(now(),hire_date)
 FROM employees
 WHERE birth_date LIKE '%-12-25'
     AND(
-    hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+        hire_date BETWEEN '1990-01-01' AND '1999-12-31'
     )
     ORDER BY birth_date, hire_date DESC;
 
