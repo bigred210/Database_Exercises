1d67814b9cd426981827e3f59964d2cb5433b577
BIGRED <RED@Jerods-MacBook-Pro.local>
2016-05-09 09:45:51 -0500

    step4 complete where continued exercises

--- where_exercises.sql
+++ where_exercises.sql
@@ -10,8 +10,9 @@ WHERE   gender = 'M'
 
 SELECT emp_no, first_name,last_name
 FROM employees
-WHERE last_name LIKE 'E%';
-
+WHERE last_name LIKE 'E%'
+    OR last_name Like '%e'
+    
 SELECT first_name, last_name
 FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
@@ -36,7 +37,7 @@ Exercises
 
 -- Now add a condition to find everybody with those names who is also male — 441 rows.
 
-Find all employees whose last name starts or ends with 'E' — 30,723 rows.
+-- Find all employees whose last name starts or ends with 'E' — 30,723 rows.
 
 Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E' — 899 rows.
 
