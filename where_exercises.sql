a1285ca49764abbdcd248c8302216566ef1b0357
BIGRED <RED@Jerods-MacBook-Pro.local>
2016-05-09 10:51:28 -0500

    step 7 complete for ORDER BY exercise

--- where_exercises.sql
+++ where_exercises.sql
@@ -20,16 +20,9 @@ WHERE birth_date LIKE '%-12-25'
     hire_date BETWEEN '1990-01-01' AND '1999-12-31'
     )
 
-
 SELECT last_name
 FROM employees
 WHERE last_name LIKE '%q%'
     AND last_name NOT LIKE '%qu%';
-
-
-
-
-
-
 
 
