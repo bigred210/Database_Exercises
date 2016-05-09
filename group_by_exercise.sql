edad9a13eb5cafdb66918c5ffb7772a644c6981a
BIGRED <RED@Jerods-MacBook-Pro.local>
2016-05-09 11:16:06 -0500

    step4 complete for GROUP BY exercise

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -5,3 +5,10 @@ FROM titles DESC;
 SELECT DISTINCT title
 FROM titles
 GROUP BY title ASC;
+
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+    AND last_name Like '%e'
+    GROUP BY last_name ASC;       
+
