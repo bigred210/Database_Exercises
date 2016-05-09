4d4a82bd0de2f5d958e15d52f609646269cc9a4f
BIGRED <RED@Jerods-MacBook-Pro.local>
2016-05-09 11:30:06 -0500

    step3 complete for LIMIT exercise

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -4,3 +4,12 @@ FROM employees
 WHERE last_name LIKE 'Z%'
 GROUP BY last_name DESC
 LIMIT 10;
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date LIKE '%-12-25'
+    AND(
+    hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+    )
+    ORDER BY birth_date, hire_date DESC
+    LIMIT 5;
