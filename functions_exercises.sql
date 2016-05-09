706c623fbc1d2de0fdde3e24a12cf4b9278ba340
BIGRED <RED@Jerods-MacBook-Pro.local>
2016-05-09 13:16:16 -0500

    step1 complete for FUNCTIONS exercise

---
+++ functions_exercises.sql
@@ -0,0 +1,41 @@
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE   gender = 'M'
+        AND(
+        first_name ='Irena'
+        OR first_name ='Vidya'
+        OR first_name ='Maya'   
+        )
+        ORDER BY last_name ASC, first_name ASC; 
+
+SELECT emp_no, first_name,last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+    AND last_name LIKE '%e'
+    ORDER BY emp_no, last_name DESC;
+    
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date LIKE '%-12-25'
+    AND(
+    hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+    )
+    ORDER BY birth_date, hire_date DESC;
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+    AND last_name NOT LIKE '%qu%';
+
+
+
+
+
+
+
+
+
+
+
