334ada82ca19fdb979f9895191c7a925ea181025
BIGRED <RED@Jerods-MacBook-Pro.local>
2016-05-09 11:22:04 -0500

    step5 complete for GROUP BY exercise

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -9,6 +9,10 @@ GROUP BY title ASC;
 SELECT DISTINCT last_name
 FROM employees
 WHERE last_name LIKE 'E%'
-    AND last_name Like '%e'
+    AND last_name LIKE '%e'
     GROUP BY last_name ASC;       
 
+SELECT DISTINCT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+    AND last_name LIKE '%e'
